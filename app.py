e2498f9c80c2ae49bb3f26da34f57eebe54221d8
aldiindrawan <45248095+aldiindrawan@users.noreply.github.com>
2020-05-23 08:40:19 +0700

    update

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ ukuran_populasi = (populasi, barang_no.shape[0]) #ukuran matriks
 print('ukuran populasi = {}'.format(ukuran_populasi))
 populasi_awal = np.random.randint(2, size = ukuran_populasi)
 populasi_awal = populasi_awal.astype(int)
-jumlah_gen = 100
+jumlah_gen = 1000
 print('populasi awal: \n{}'.format(populasi_awal))
 
 #fungsi untuk menghitung nilai fitness
@@ -46,51 +46,6 @@ def get_fitness(berat,  profit, populasi, max_tas):
             fitness[i] = 0
     return fitness.astype(int)
 
-#fungsi seleksi
-def selection(fitness, no_parent, populasi):
-    fitness = list(fitness)
-    parent = np.empty((no_parent, populasi.shape[1]))
-    for i in range(no_parent):
-        index_maxfitness = np.where(fitness == np.max(fitness)) #index fitness dengan nilai terbesar
-        parent[i,:] = populasi[index_maxfitness[0][0], :]
-        fitness[index_maxfitness[0][0]] = -999999
-    return parent
-
-#fungsi crossover
-def crossover(parent, no_offspring):
-    offspring = np.empty((no_offspring, parent.shape[1]))
-    titik_cross = int(parent.shape[1]/2)
-    rate = 0.8
-    i=0
-    while (parent.shape[0] < no_offspring):
-        index_parent1 = i%parent.shape[0]
-        index_parent2 = (i+1)%parent.shape[0]
-        x = rd.random()
-        if x > rate:
-            continue
-        index_parent1 = i%parent.shape[0]
-        index_parent2 = (i+1)%parent.shape[0]
-        offspring[i,0:titik_cross] = parent[index_parent1,0:titik_cross]
-        offspring[i,titik_cross:] = parent[index_parent2,titik_cross:]
-        i=+1
-    return offspring
-
-#fungsi mutasi
-def mutation(offspring):
-    mutant = np.empty((offspring.shape))
-    rate = 0.4
-    for i in range(mutant.shape[0]):
-        nilai_random = rd.random()
-        mutant[i,:] = offspring[i,:]
-        if nilai_random > rate:
-            continue
-        new_nilai_random = randint(0,offspring.shape[1]-1)
-        if mutant[i,new_nilai_random] == 0 :
-            mutant[i,new_nilai_random] = 1
-        else :
-            mutant[i,new_nilai_random] = 0
-    return mutant
-
 #fungsi optimize
 def optimize(berat, profit, populasi, ukuran_populasi, no_generasi, max_tas):
     parameter, catatan_fitness = [], []
@@ -98,12 +53,55 @@ def optimize(berat, profit, populasi, ukuran_populasi, no_generasi, max_tas):
     no_offspring = ukuran_populasi[0] - no_parent
     for i in range(no_generasi):
         fitness = get_fitness(berat, profit, populasi, max_tas)
-        catatan_fitness.append(fitness)
-        parent = selection(fitness, no_parent, populasi)
-        offspring = crossover(parent, no_offspring)
-        mutant = mutation(offspring)
+
+        #code untuk proses seleksi
+        fitness = list(fitness)
+        parent = np.empty((no_parent, populasi.shape[1]))
+        for i in range(no_parent):
+            index_maxfitness = np.where(fitness == np.max(fitness)) #index fitness dengan nilai terbesar
+            parent[i,:] = populasi[index_maxfitness[0][0], :]
+            fitness[index_maxfitness[0][0]] = -999999
+        #akhir code seleksi
+
+        #code untuk proses crosover
+        offspring = np.empty((no_offspring, parent.shape[1]))
+        titik_cross = int(parent.shape[1]/2)
+        rate = 0.8
+        i=0
+        while (parent.shape[0] < no_offspring):
+            index_parent1 = i%parent.shape[0]
+            index_parent2 = (i+1)%parent.shape[0]
+            x = rd.random()
+            if x > rate:
+                continue
+            index_parent1 = i%parent.shape[0]
+            index_parent2 = (i+1)%parent.shape[0]
+            offspring[i,0:titik_cross] = parent[index_parent1,0:titik_cross]
+            offspring[i,titik_cross:] = parent[index_parent2,titik_cross:]
+            i=+1
+        #akhir code crossover
+
+        #code untuk proses mutasi
+        mutant = np.empty((offspring.shape))
+        rate = 0.4
+        for i in range(mutant.shape[0]):
+            nilai_random = rd.random()
+            mutant[i,:] = offspring[i,:]
+            if nilai_random > rate:
+                continue
+            new_nilai_random = randint(0,offspring.shape[1]-1)
+            if mutant[i,new_nilai_random] == 0 :
+                mutant[i,new_nilai_random] = 1
+            else :
+                mutant[i,new_nilai_random] = 0
+        #akhir code mutasi
+
+        #code populasi baru
         populasi[0:parent.shape[0], :] = parent
         populasi[parent.shape[0]:, :] = mutant
+
+        #tambah data ftines kedalam catatan
+        catatan_fitness.append(fitness)
 
     print('generasi terakhir: \n{}\n'.format(populasi))
     fitness_gen_terakhir = get_fitness(berat, profit, populasi, max_tas)
@@ -114,22 +112,20 @@ def optimize(berat, profit, populasi, ukuran_populasi, no_generasi, max_tas):
 
 #main
 parameter, fitness_history = optimize(berat, profit, populasi_awal, ukuran_populasi, jumlah_gen, max_tas)
-print('Optimized parameter  \n{}'.format(parameter))
+print('Hasil Akhir  \n{}'.format(parameter))
 selected_items = barang_no * parameter
 print('\nItem terpilih untuk dimasukkan ke dalam tas : ')
 for i in range(selected_items.shape[1]):
   if selected_items[0][i] != 0:
      print('{}\n'.format(selected_items[0][i]))
 
-
-#plot
+#grafik
 fitness_history_mean = [np.mean(fitness) for fitness in fitness_history]
 fitness_history_max = [np.max(fitness) for fitness in fitness_history]
-plt.plot(list(range(jumlah_gen)), fitness_history_mean, label = 'Mean Fitness')
-plt.plot(list(range(jumlah_gen)), fitness_history_max, label = 'Max Fitness')
+plt.plot(list(range(jumlah_gen)), fitness_history_mean, label = 'rata-rata Fitness')
+plt.plot(list(range(jumlah_gen)), fitness_history_max, label = 'Fitness maksimum')
 plt.legend()
-plt.title('Fitness through the generations')
-plt.xlabel('Generations')
+plt.title('nilai fitnes setiap generasi')
+plt.xlabel('Generasi')
 plt.ylabel('Fitness')
 plt.show()
-print(np.asarray(fitness_history).shape)
